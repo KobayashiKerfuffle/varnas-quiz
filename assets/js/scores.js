1f5e10ef1f5a84138ddf8ae2d8380359c5d057f7
Peter Sadauskas <peter.sadauskas@gmail.com>
2023-10-05 15:32:02 +1030

    Made final updates to match question formatting with client requirements.

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,17 +1,31 @@
 function printHighscores() {
-  // either get scores from localstorage or set to empty array
+  // Get scores from localstorage or set to an empty array if null
+  var highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
-  // sort highscores by score property in descending order
+  // Sort highscores by score property in descending order
+  highscores.sort(function(a, b) {
+    return b.score - a.score;
+  });
 
-  for (var i = 0; i < highscores.length; i += 1) {
-    // create li tag for each high score
-    // display on page
+  for (var i = 0; i < highscores.length; i++) {
+    // Create li tag for each high score
+    var liTag = document.createElement("li");
+    liTag.textContent = highscores[i].initials + " - " + highscores[i].score;
+
+    // Display on page
+    document.getElementById("highscores").appendChild(liTag);
   }
 }
 
-function clearHighscores() {}
+function clearHighscores() {
+  // Clear the scores from local storage
+  localStorage.removeItem("highscores");
+  
+  // Clear scores from the page
+  document.getElementById("highscores").innerHTML = "";
+}
 
 document.getElementById("clear").onclick = clearHighscores;
 
-// run function when page loads
+// Run function when the page loads
 printHighscores();
